d7923692312f0f5279de5935cb407e8a2e310bf4
Ashster <ashster@ashster.europe-west2-c.c.ash-claw-487713.internal>
2026-03-14 14:04:29 +0000

    feat(epl): add ingest scrapers for standings fixtures and form

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,37 +1,10 @@
-"""
-Public scraper package exports.
-"""
+"""Public scraper package exports."""
 
-
-def get_last5_form():
-    from .teamrankings import get_last5_form as _get_last5_form
-
-    return _get_last5_form()
-
-
-def get_advanced_stats():
-    from .nba_stats import get_advanced_stats as _get_advanced_stats
-
-    return _get_advanced_stats()
-
-
-def get_four_factors():
-    from .nba_stats import get_four_factors as _get_four_factors
-
-    return _get_four_factors()
-
-
-def get_defense_stats():
-    from .nba_stats import get_defense_stats as _get_defense_stats
-
-    return _get_defense_stats()
-
-
-def get_injury_report():
-    from .injury_report import get_injury_report as _get_injury_report
-
-    return _get_injury_report()
-
+from .teamrankings import get_last5_form
+from .nba_stats import get_advanced_stats, get_four_factors, get_defense_stats
+from .injury_report import get_injury_report
+from .epl_fixtures_results import get_epl_fixtures_results, get_epl_results, get_epl_fixtures
+from .epl_standings_form import get_epl_standings_form
 
 __all__ = [
     "get_last5_form",
@@ -39,4 +12,8 @@ __all__ = [
     "get_four_factors",
     "get_defense_stats",
     "get_injury_report",
+    "get_epl_fixtures_results",
+    "get_epl_results",
+    "get_epl_fixtures",
+    "get_epl_standings_form",
 ]
